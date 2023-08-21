e60d9e69171f4adfb14700ca302ad2ac1916f524
MrPro78 <ethandaboi@gmail.com>
2023-08-22 02:33:50 +1000

    successfully created playround function had to remove the input to outside the function as it was creating an initial blank response managed to compress the conditionals with 3 winning conditionals and inversing the opposite with a simple else afterwards

--- game.js
+++ game.js
@@ -1,10 +1,29 @@
 function getComputerChoice() {
-    const  a = "rock";
-    const b = "scissors";
-    const c = "paper"; 
-    let randomise_group = [a, b, c]; 
+    const choices = ["rock", "paper", "scissors"]; 
+    let randomise_group = choices; 
     return randomise_group[Math.floor(Math.random() * randomise_group.length)];
 }
 
-let check = getComputerChoice();
-console.log(check);
+
+function playRound(playerSelection, computerSelection) {
+
+    computerSelection = getComputerChoice(); 
+
+    // toLowerCase() function converst all inputs to lowercase, this is to ensure case insensitivity// 
+
+   if(playerSelection === computerSelection) {
+    return "Its a tie!"; 
+   } else if(
+    (playerSelection === "rock" && computerSelection === "scissors") || 
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+   ){
+    return "Player wins!"; 
+   } else {
+    return "Computer wins!"; 
+   }
+}
+
+const player = prompt("what gesture do you choose?:").toLowerCase();  
+comp = getComputerChoice(); 
+console.log(playRound(player, comp)); 
