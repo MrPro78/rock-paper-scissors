0fc2d0503e58ce0b836792b9f5204e06ab132f50
MrPro78 <ethandaboi@gmail.com>
2023-09-02 14:48:06 +1000

    added a scoring system

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 let playerScore = 0; 
 let computerScore = 0; 
-let tieScore = 0;
+
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"]; 
@@ -11,34 +11,43 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-    //calling computerchoice function/// 
-    computerSelection = getComputerChoice(); 
+    console.log(playerSelection); 
+    console.log(computerSelection); 
+
+   
     winner = "";
 
    if(playerSelection === computerSelection) {
     winner = "Its a tie!"; 
+    computerScore = computerScore; 
+    playerScore = playerScore; 
    } else if(
     (playerSelection === "rock" && computerSelection === "scissors") || 
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
    ){
     winner = "Player wins!";
+    playerScore++; 
    
    } else {
     winner = "Computer wins!";
+    computerScore++; 
    }
+
+   console.log(playerScore); 
+   console.log(computerScore); 
 
    return console.log(winner); 
 }
 
-function game() {     
+function game() {   
     for(i=0; i < 5; i++){
+        
         let playerChoice = prompt("what gesture do you choose?:").toLowerCase();  
         let compChoice = getComputerChoice(); 
         // toLowerCase() function converst all inputs to lowercase, this is to ensure case insensitivity//         
         playRound(playerChoice, compChoice); 
-        
-    }
+    }; 
 }
 
-console.log(game()); 
+game();
